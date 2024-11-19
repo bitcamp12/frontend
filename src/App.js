498e163d6a6c0614ad7d07139e5880f7cf683e18
rwanda95do <dlsdnr2379@naver.com>
2024-11-19 19:35:12 +0900

    feat(mypage):마이페이지프론트작업중

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Main from './components/Main';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { BrowserRouter } from 'react-router-dom';
-
+import React from "react";
+import Main from "./components/Main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter } from "react-router-dom";
+import Info from "./components/mypage/Info";
 
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Main />
+            </BrowserRouter>
+        </>
+    );
 };
 
 export default App;
