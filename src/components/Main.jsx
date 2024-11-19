9f83345e84694d61b561fc2dcd4afbd213fad0d8
imnotminda <dmc101110@gmail.com>
2024-11-19 15:28:08 +0900

    feat/style/refactor (user main): user front v1.01

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../assets/css/Main.css";
+import "../assets/css/Buttons.css"
 import React from "react";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import AdvertiseContainer from "./AdvertiseContainer";
-import ButtonNavContainer from "./ButtonNavContainer";
-import SaleContainer from "./SaleContainer";
-import BarNavContainer from "./BarNavContainer";
-import InfiniteScrollContainer from "./InfiniteScrollContainer";
+import AdvertiseContainer from "./main/AdvertiseContainer";
+import ButtonNavContainer from "./main/ButtonNavContainer";
+import SaleContainer from "./main/SaleContainer";
+import BarNavContainer from "./main/BarNavContainer";
+import InfiniteScrollContainer from "./main/InfiniteScrollContainer";
 
 const Main = () => {
   return (
-    <div>
+    <>
       <AdvertiseContainer />
       <ButtonNavContainer />
       <SaleContainer />
       <BarNavContainer />
-      <ButtonNavContainer />
       <InfiniteScrollContainer />
-    </div>
+    </>
   );
 };
 
---
+++ src/components/main/InfiniteScrollContainer.jsx
@@ -0,0 +1,59 @@
+import "../../assets/css/Main.css";
+
+import React, { useEffect, useRef, useState } from 'react';
+
+const InfiniteScrollContainer = () => {
+    const [items, setItems] = useState(Array.from({ length: 6 }));
+    const target = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0,
+        };
+
+        const callback = (entries) => {
+            if (entries[0].isIntersecting) {
+                loadMoreItems();
+            }
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+        if (target.current) {
+            observer.observe(target.current);
+        }
+
+        return () => {
+            if (target.current) {
+                observer.unobserve(target.current);
+            }
+        };
+    }, []);
+
+    const loadMoreItems = () => {
+        setItems((prevItems) => [
+            ...prevItems,
+            ...Array.from({ length: 6 }),
+        ]);
+    };
+    return (
+        <div>
+        <div className="infinite-scroll-container" id="infinite-scroll-container">
+            {items.map((_, index) => (
+                <div className="infinite-scroll-card" key={index}>
+                    <img src="https://ticketimage.interpark.com/Play/image/large/21/21013249_p.gif" />
+                    <div className="infinite-scroll-card-body">
+                        <h3>제목 {index + 1}</h3>
+                        <h6 className="infinite-scroll-card-info">장소</h6>
+                        <h6 className="infinite-scroll-card-info">시간</h6>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <div ref={target} style={{ height: "100px", backgroundColor: "red" }}></div>
+    </div>
+    );
+};
+
+export default InfiniteScrollContainer;
