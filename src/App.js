aa55d205dc8052ad2e2f1a1fa9494347f2991caf
unknown <tkp1200@naver.com>
2024-11-19 15:10:32 +0900

    main/foor제작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import MainNa from './components/MainNa';
+import Footer from './components/Footer';
+import PlayDetail from './components/PlayDetail';
 const App = () => {
   return (
     <div>
-      
+      <MainNa/>
+      <PlayDetail/>
+      <Footer/>
     </div>
   );
 };
