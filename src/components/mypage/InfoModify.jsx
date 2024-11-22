763754d5cdf59a839e2cc56144614cb05a73452b
rwanda95do <dlsdnr2379@naver.com>
2024-11-22 17:24:00 +0900

    feature(mypage):정보수정 js, 데이터불러오기

--- src/components/mypage/InfoModify.jsx
+++ src/components/mypage/InfoModify.jsx
@@ -1,9 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Icon from "./Icon";
 
 import styles from "../../assets/css/mypage/InfoModify.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const InfoModify = () => {
+    // --수정 버튼을 누르면 보였다 안보였다--------------
+    // --핸드폰번호수정--
+    const [phoneToggle, isPhoneToggle] = useState(false); // false이면 안보이게 하고 싶어요
+    const editPhoneBtnToggle = () => {
+        isPhoneToggle(!phoneToggle);
+    };
+    const [phoneAuthToggle, isPhoneAuthToggle] = useState(false);
+    const editPhoneAuthBtnToggle = (e) => {
+        e.preventDefault();
+        isPhoneAuthToggle(!phoneAuthToggle);
+        console.log(phoneAuthToggle);
+    };
+    // --이메일 수정--
+    const [emailToggle, isEmailToggle] = useState(false);
+    const editEmailBtnToggle = () => {
+        isEmailToggle(!emailToggle);
+    };
+    const [emailAuthToggle, isEmailAuthToggle] = useState(false);
+    const editEmailAuthBtnToggle = (e) => {
+        e.preventDefault();
+        isEmailAuthToggle(!emailAuthToggle);
+        console.log(emailAuthToggle);
+
+        // 
+    };
+    // --------------
+    const [allCheck, setAllCheck] = useState(false);
+    const [itemCheck, setCheckItem] = useState({
+        smsCheck: false,
+        emailCheck: false,
+    });
+    const handleAllCheck = (e) => {
+        const check = e.target.checked;
+
+        setAllCheck(check);
+        setCheckItem({
+            smsCheck: check,
+            emailCheck: check,
+        });
+    };
+    const handleItemCheck = (e) => {
+        console.log(e.target.id);
+        const { id, checked } = e.target;
+
+        setCheckItem((prev) => {
+            const newItemCheck = {
+                ...prev,
+                [id]: checked,
+            };
+            setAllCheck(newItemCheck.smsCheck && newItemCheck.emailCheck);
+            return newItemCheck;
+        });
+    };
+
+    // --------------
+    const [data, setDate] = useState(null);
+
+    useEffect(() => {
+        // 세션에서 id를 가져옵니다.
+        const userId = "apple"; //sessionStorage.getItem("id");
+
+        axios
+            .get("http://localhost:8080/api/members/getUserInfo", {
+                params: { id: userId },
+            })
+            .then((response) => {
+                console.log(response);
+                setDate(response.data);
+            })
+            .catch((error) => console.error("ERROR(사용자정보수정): ", error));
+    }, []);
+
+    if (!data) {
+        // data(id)가 없다면 메인페이지 또는 회원가입페이지로 이동 시키고 싶은데..
+        return <div>No data available</div>;
+    }
+
     return (
         <div className={styles.member_info_modify}>
             <h3>회원정보수정</h3>
@@ -13,120 +92,168 @@ const InfoModify = () => {
                     <h5>기본정보</h5>
                     <dl>
                         <dt>아이디</dt>
-                        <dd>hong123</dd>
+                        <dd>{data.id}</dd>
                     </dl>
                     <dl>
                         <dt>이름</dt>
                         <dd>
-                            <span>홍길동</span>
-                            <a className={styles.whiteBtn}>수정</a>
+                            <span>{data.name}</span>
+                            <a href="" className={styles.whiteBtn}>
+                                수정
+                            </a>
                         </dd>
                     </dl>
                     <dl>
                         <dt>휴대폰번호</dt>
                         <dd>
-                            <span>010-0000-0000</span>
-                            <a className={styles.whiteBtn}>수정</a>
+                            <span>{data.phone}</span>
+                            <a
+                                id="editPhoneBtn"
+                                className={styles.whiteBtn}
+                                onClick={editPhoneBtnToggle}
+                            >
+                                수정
+                            </a>
 
-                            <div className={styles.modifyEnter}>
-                                <p>휴대폰번호 변경을 위해 인증이 필요합니다.</p>
-                                <div>
-                                    <div className={styles.enterBox}>
-                                        <input
-                                            type="text"
-                                            name=""
-                                            id=""
-                                            placeholder="변경 휴대폰번호입력 (-없이)"
-                                        />
-                                        <div className={styles.iconWrap}>
-                                            <Icon
-                                                name="closeCircle"
-                                                size={20}
-                                                color="gray"
+                            {phoneToggle && (
+                                <div
+                                    id="editPhoneDiv"
+                                    className={styles.modifyEnter}
+                                >
+                                    <p>
+                                        휴대폰번호 변경을 위해 인증이
+                                        필요합니다.
+                                    </p>
+                                    <div>
+                                        <div className={styles.enterBox}>
+                                            <input
+                                                type="text"
+                                                name=""
+                                                id=""
+                                                placeholder="변경 휴대폰번호입력 (-없이)"
                                             />
-                                        </div>
-                                        <div className={styles.checkDiv}>
-                                            잘못된 휴대폰 형식입니다.
-                                        </div>
-                                    </div>
-                                    <div className={`${styles.enterBox}`}>
-                                        {/* 인증번호 요청 버튼을 누르면 이건 보이게 */}
-                                        <input type="text" name="" id="" />
-                                        <div className={styles.iconWrap}>
-                                            남은시간:3:00
+                                            <div className={styles.iconWrap}>
+                                                <Icon
+                                                    name="closeCircle"
+                                                    size={20}
+                                                    color="gray"
+                                                />
+                                            </div>
+                                            <div className={styles.checkDiv}>
+                                                잘못된 휴대폰 형식입니다.
+                                            </div>
                                         </div>
                                         <button
                                             className={`${styles.violetBtn} ${styles.w100}`}
+                                            onClick={editPhoneAuthBtnToggle}
                                         >
-                                            인증
+                                            인증번호요청
                                         </button>
+                                        {phoneAuthToggle && (
+                                            <div
+                                                className={`${styles.enterBox}`}
+                                            >
+                                                {/* 인증번호 요청 버튼을 누르면 이건 보이게 */}
+                                                <input
+                                                    type="text"
+                                                    name=""
+                                                    id=""
+                                                />
+                                                <div
+                                                    className={styles.iconWrap}
+                                                >
+                                                    남은시간:3:00
+                                                </div>
+                                                <button
+                                                    className={`${styles.violetBtn} ${styles.w100}`}
+                                                >
+                                                    인증
+                                                </button>
+                                            </div>
+                                        )}
                                     </div>
-                                    <button
-                                        className={`${styles.violetBtn} ${styles.w100}`}
-                                    >
-                                        {/* 인증번호 요청 버튼을 누르면 dispaly:none속성을 축하는 클래스 추가 */}
-                                        인증번호요청
-                                    </button>
                                 </div>
-                            </div>
+                            )}
                         </dd>
                     </dl>
                     <dl>
                         <dt>이메일</dt>
                         <dd>
-                            <span>hong@naver.com</span>
-                            <a className={styles.whiteBtn}>수정</a>
+                            <span>{data.email}</span>
+                            <a
+                                href="#"
+                                className={styles.whiteBtn}
+                                onClick={editEmailBtnToggle}
+                            >
+                                수정
+                            </a>
 
-                            <div className={styles.modifyEnter}>
-                                <p>이메일 변경을 위해 인증이 필요합니다.</p>
-                                <div>
-                                    <div className={styles.enterBox}>
-                                        <input
-                                            type="text"
-                                            name=""
-                                            id=""
-                                            placeholder="변경 이메일"
-                                        />
-                                        <div className={styles.iconWrap}>
-                                            <Icon
-                                                name="closeCircle"
-                                                size={20}
-                                                color="gray"
+                            {emailToggle && (
+                                <div className={styles.modifyEnter}>
+                                    <p>이메일 변경을 위해 인증이 필요합니다.</p>
+                                    <div>
+                                        <div className={styles.enterBox}>
+                                            <input
+                                                type="text"
+                                                name="newEmail"
+                                                id="newEmail"
+                                                placeholder="변경 이메일"
                                             />
+                                            <div className={styles.iconWrap}>
+                                                <Icon
+                                                    name="closeCircle"
+                                                    size={20}
+                                                    color="gray"
+                                                />
+                                            </div>
+                                            <select name="" id="">
+                                                <option value="">
+                                                    직접입력
+                                                </option>
+                                                <option value="">
+                                                    naver.com
+                                                </option>
+                                                <option value="">
+                                                    gmail.com
+                                                </option>
+                                            </select>
+                                            <div className={styles.checkDiv}>
+                                                이메일을 입력해주세요
+                                            </div>
+                                            <button
+                                                className={`${styles.violetBtn} ${styles.w100}`}
+                                                onClick={editEmailAuthBtnToggle}
+                                            >
+                                                인증번호요청
+                                            </button>
                                         </div>
-                                        <select name="" id="">
-                                            <option value="">직접입력</option>
-                                            <option value="">naver.com</option>
-                                            <option value="">gmail.com</option>
-                                        </select>
-                                        <div className={styles.checkDiv}>
-                                            이메일을 입력해주세요
-                                        </div>
+                                        {emailAuthToggle && (
+                                            <div className={styles.enterBox}>
+                                                <input
+                                                    type="text"
+                                                    name=""
+                                                    id=""
+                                                />
+                                                <div
+                                                    className={styles.iconWrap}
+                                                >
+                                                    남은시간:3:00
+                                                </div>
+                                                <button
+                                                    className={`${styles.violetBtn} ${styles.w100}`}
+                                                >
+                                                    인증
+                                                </button>
+                                            </div>
+                                        )}
                                     </div>
-                                    <div className={styles.enterBox}>
-                                        <input type="text" name="" id="" />
-                                        <div className={styles.iconWrap}>
-                                            남은시간:3:00
-                                        </div>
-                                        <button
-                                            className={`${styles.violetBtn} ${styles.w100}`}
-                                        >
-                                            인증
-                                        </button>
-                                    </div>
-                                    <button
-                                        className={`${styles.violetBtn} ${styles.w100}`}
-                                    >
-                                        {/* 인증번호 요청 버튼을 누르면 dispaly:none속성을 축하는 클래스 추가 */}
-                                        인증번호요청
-                                    </button>
                                 </div>
-                            </div>
+                            )}
                         </dd>
                     </dl>
                     <dl>
-                        <dt>생년월일</dt>
-                        <dd>2024-11-20</dd>
+                        <dt>가입일</dt>
+                        <dd>{data.registerDate}</dd>
                     </dl>
                     <dl>
                         <dt>성별</dt>
@@ -144,16 +271,36 @@ const InfoModify = () => {
                     <h5>정보 수신 동의</h5>
                     <ul>
                         <li>
-                            <input type="checkbox" name="" id="" />
+                            <input
+                                type="checkbox"
+                                name=""
+                                id="allCheck"
+                                checked={allCheck}
+                                onChange={handleAllCheck}
+                            />
                             <label htmlFor="">이벤트, 쇼핑혜택 수신동의</label>
                         </li>
                         <ul>
                             <li>
-                                <input type="checkbox" name="" id="" />
+                                <input
+                                    type="checkbox"
+                                    name=""
+                                    id="smsCheck"
+                                    className="check"
+                                    checked={itemCheck.smsCheck}
+                                    onChange={handleItemCheck}
+                                />
                                 <label htmlFor="">SMS 수신</label>
                             </li>
                             <li>
-                                <input type="checkbox" name="" id="" />
+                                <input
+                                    type="checkbox"
+                                    name=""
+                                    id="emailCheck"
+                                    className="check"
+                                    checked={itemCheck.emailCheck}
+                                    onChange={handleItemCheck}
+                                />
                                 <label htmlFor="">이메일 수신</label>
                             </li>
                             <li>
