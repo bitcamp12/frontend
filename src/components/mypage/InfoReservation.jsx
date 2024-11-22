ef35db8863ebbe51a5c676d0c8125baf44abf695
imnotminda <dmc101110@gmail.com>
2024-11-22 19:45:31 +0900

    style user css (user myPage)

--- src/components/mypage/InfoReservation.jsx
+++ src/components/mypage/InfoReservation.jsx
@@ -3,61 +3,65 @@ import React from 'react';
 import styles from "../../assets/css/mypage/InfoReservation.module.css"
 const InfoReservation = () => {
     return (
-        <div class={styles.infoReservation}>
-            <h3>예매내역 확인 · 취소</h3>
-            <p class={styles.memo}>
-            * 모든 내역을 합쳐 <span>0건</span>의 예매내역이 있습니다.
-            </p>
-            <div class={styles.searchSection}>
+        <div className={styles.infoReservation}>
+        <h3>예매내역 확인 · 취소</h3>
+        <p className={styles.memo}>
+          * 모든 내역을 합쳐 <span>0건</span>의 예매내역이 있습니다.
+        </p>
+        <div className={styles.searchSection}>
+            <div className={styles.searchSectionTime}>
                 <label>조회기간 선택:</label>
                 <button>15일</button>
                 <button>1개월</button>
                 <button>3개월</button>
-                <select>
-                    <option>주문일자별</option>
-                    <option>예매일</option>
-                </select>
-                <select>
-                    <option>년</option>
-                </select>
-                <select>
-                    <option>월</option>
-                </select>
-                <button>검색</button>
             </div>
-            <div class={styles.tableContainer}>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>예매일</th>
-                        <th>예약번호</th>
-                        <th>상품명</th>
-                        <th>이용일/매수</th>
-                        <th>취소가능일</th>
-                        <th>현재상태</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td colspan="6">설정된 기간에 맞는 예매 내역이 없습니다.</td>
-                    </tr>
-                    </tbody>
-                    <tfoot></tfoot>
-                </table>
+            <div className={styles.searchSectionDate}>
+            <select>
+                <option>주문일자별</option>
+                <option>예매날짜</option>
+            </select>
+            <select>
+                <option>년</option>
+                <option>2024</option>
+            </select>
+            <select>
+                <option>월</option>
+                <option>11</option>
+            </select>
+            <button>검색</button>
             </div>
-
-            <div>페이징</div>
-
-            <p class={styles.note}>
-            <span>※ [상세보기]에서 예매 상세내역 확인 및 예매 취소를 하실 수 있습니다.</span><br/>
-            <span>티켓이 배송된 이후에는 인터넷 취소가 안되며, 배송받으신 티켓의 취소는 이용일까지 인터파크로 접수된 이후에 취소가능합니다.</span>
-            </p>
-
-            <div className={styles.btnWrap}>
-                <button class={styles.violetBtn}>패키지/시즌권 부분환불 신청</button>
-            </div>
+        </div>
+        <div className={styles.listContainer}>
+          <div className={styles.listHeader}>
+            <span>예매날짜</span>
+            <span>예약번호</span>
+            <span>상품명</span>
+            <span>이용일/매수</span>
+            <span>취소가능일</span>
+            <span>현재상태</span>
+          </div>
+          <div className={styles.listItem}>
+            <span className={styles.fullRow}>
+              설정된 기간에 맞는 예매 내역이 없습니다.
+            </span>
+          </div>
+        </div>
+        <div className={styles.pagination}>페이징</div>
+        <p className={styles.note}>
+          <span>
+            ※ [상세보기]에서 예매 상세내역 확인 및 예매 취소를 하실 수 있습니다.
+          </span>
+          <br />
+          <span>
+            티켓이 배송된 이후에는 인터넷 취소가 안되며, 배송받으신 티켓의 취소는
+            이용일까지 인터파크로 접수된 이후에 취소가능합니다.
+          </span>
+        </p>
+        <div className={styles.btnWrap}>
+          <button className={styles.violetBtn}>패키지/시즌권 부분환불 신청</button>
+        </div>
       </div>
     );
-};
-
-export default InfoReservation;
+  };
+  
+  export default InfoReservation;
