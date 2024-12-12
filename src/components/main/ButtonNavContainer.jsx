7dd79ce86cc9ba4dc11036f7d25c47cb45396271
imnotminda <dmc101110@gmail.com>
2024-12-12 14:35:18 +0900

    feature user main 좌석예매

--- src/components/main/ButtonNavContainer.jsx
+++ src/components/main/ButtonNavContainer.jsx
@@ -6,32 +6,57 @@ import "../../assets/css/Main.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { HashLink } from 'react-router-hash-link';
 
-const ButtonNavContainer = ({setSelectedTab}) => {
+const ButtonNavContainer = ({ setSelectedTab }) => {
+  const handleScroll = () => {
+    const target = document.getElementById("bar-nav-container");
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="button-nav-container">
-      <HashLink to="#bar-nav-container">
-        <button type="button" className="btn btn-secondary btn-lg custom-btn" onClick={() => setSelectedTab(0)}>
-          연극 전체보기
-        </button>
-      </HashLink>
-      <HashLink to="#bar-nav-container">
-        <button type="button" className="btn btn-secondary btn-lg custom-btn" onClick={() => setSelectedTab(1)}>
-         ⏰ 곧 마감
-        </button>
-      </HashLink>
-      <HashLink to="#bar-nav-container">
-        <button type="button" className="btn btn-secondary btn-lg custom-btn" onClick={() => setSelectedTab(2)}>
-          🗓️ 상영예정
-        </button>
-      </HashLink>
-      <HashLink to="#bar-nav-container">
-        <button type="button" className="btn btn-secondary btn-lg custom-btn" onClick={() => setSelectedTab(3)}>
-          ⭐️ 프리미엄
-        </button>
-      </HashLink>
+      <button
+        type="button"
+        className="btn btn-secondary btn-lg custom-btn"
+        onClick={() => {
+          setSelectedTab(0);
+          handleScroll();
+        }}
+      >
+        연극 전체보기
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary btn-lg custom-btn"
+        onClick={() => {
+          setSelectedTab(1);
+          handleScroll();
+        }}
+      >
+        ⏰ 곧 마감
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary btn-lg custom-btn"
+        onClick={() => {
+          setSelectedTab(2);
+          handleScroll();
+        }}
+      >
+        🗓️ 상영예정
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary btn-lg custom-btn"
+        onClick={() => {
+          setSelectedTab(3);
+          handleScroll();
+        }}
+      >
+        ⭐️ 프리미엄
+      </button>
     </div>
   );
 };
