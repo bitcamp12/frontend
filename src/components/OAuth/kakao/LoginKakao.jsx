f64f0bff2c2b1518a4bb492ca3902eeb33cda75e
unknown <tkp1200@naver.com>
2024-12-20 11:19:29 +0900

    중간 저장

--- src/components/OAuth/kakao/LoginKakao.jsx
+++ src/components/OAuth/kakao/LoginKakao.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const LoginKakao = () => {
     const restApikey = "e838336ee6195b503a57e0fd87698d96";
-
-const redirectUrl = "http://localhost:3000";
+const redirectUrl = "http://localhost:8080/api/login/kakao";
 
 const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${restApikey}&redirect_uri=${redirectUrl}&response_type=code`;
 const loginHandler = () => {
@@ -15,7 +14,7 @@ const loginHandler = () => {
         </div>
     );
 };
-
+//http://localhost:3000/?code=q7CMqQde0AmZ8HUcri91aeTDJsGbs2Zs2yuu1DyhMZDgrQ-ic7TILQAAAAQKPCQgAAABk9I-lgSvm_uHqQwxKA
 export default LoginKakao;
 
 
