0b763761b2803288f3903c8ad74b1a0432a67e2b
luvoice97 <luvoice97@gmail.com>
2024-12-24 14:32:00 +0900

    1

--- src/components/OAuth/Google/LoginGoogle.jsx
+++ src/components/OAuth/Google/LoginGoogle.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AppleLogo from "../../../assets/images/Google.png";
 const LoginGoogle = () => {
     const restApikey = "332810267931-3v4gikheu6j8j2l1egm5to3o3opa57lf.apps.googleusercontent.com";
-    const redirectUrl = `${process.env.REACT_FRONT_API_URL}/googleloding`;
+    const redirectUrl = `https://www.30ticket.shop/googleloding`;
 
     const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${restApikey}&scope=openid%20profile%20email&redirect_uri=${redirectUrl}`;
 const loginHandler = () => {
