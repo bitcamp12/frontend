7146bdeb6b6e956655ada52b7677fc365e3f8e84
unknown <tkp1200@naver.com>
2024-12-24 10:04:20 +0900

    diq

--- src/components/OAuth/Naver/LoginNaver.jsx
+++ src/components/OAuth/Naver/LoginNaver.jsx
@@ -3,7 +3,7 @@ import NaverLogo from "../../../assets/images/Naver.png";
 const LoginNaver = () => {
 
     const naverClientId = `fMDjfAFks4nyz8Itoz5_`; // 환경 변수로 설정
-    const naverCallbackUrl = `${process.env.REACT_APP_API_URL}/naverloding`;
+    const naverCallbackUrl = `${process.env.REACT_FRONT_API_URL}/naverloding`;
     const naverClientSecret =`rCrJLLYYoK`;
     const naverAuthUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientId}&state=${naverClientSecret}&redirect_uri=${naverCallbackUrl}`;
     const loginHandler = () => {
